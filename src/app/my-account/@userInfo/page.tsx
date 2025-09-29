0bb1cd85fe18bf50e018d98d8e1246bd198d21d4
meggsan <meggie.sanchez@gmail.com>
2025-09-29 18:06:11 +0200

    test page as dynamic

--- src/app/my-account/@userInfo/page.tsx
+++ src/app/my-account/@userInfo/page.tsx
@@ -1,5 +1,7 @@
 import { validateAccessToken } from "utils/auth/validateAccessToken";
 
+export const dynamic = "force-dynamic";
+
 export default async function MyAccountPage() {
   const customer = await validateAccessToken();
 
@@ -10,8 +12,8 @@ export default async function MyAccountPage() {
   return (
     <div>
       <section>
-        <h2>Your info</h2>
-        <h1>Welcome {customer.firstName}</h1>
+        <h2>Account info</h2>
+        <h1>Welcome: {customer.firstName}</h1>
         <p>email: {customer.email}</p>
       </section>
     </div>
